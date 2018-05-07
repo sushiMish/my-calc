6fa84ca426cf7b68a3b6ab5e035b5fc7581fe58a
Kumari <Sushila.Kumari@unisys.com>
2018-05-07 22:46:20 +0530

    Add clear button functionality

--- src/App.js
+++ src/App.js
@@ -7,44 +7,57 @@ class App extends React.Component {
   handleClick = () => {
     let in1 = parseInt(this.input1.value, 10);
     let in2 = parseInt(this.input2.value, 10);
-    switch(this.op.value){
-      case '+': this.setState({result: in1 + in2});
+    switch (this.op.value) {
+      case '+': this.setState({ result: in1 + in2 });
         break;
-      case '-': this.setState({result:in1-in2});
+      case '-': this.setState({ result: in1 - in2 });
         break;
-      case 'x': this.setState({result:in1*in2});
+      case 'x': this.setState({ result: in1 * in2 });
         break;
-      case '/': this.setState({result:in1/in2});
+      case '/': this.setState({ result: in1 / in2 });
         break;
-    } 
+    }
   }
 
-  constructor(){
+  handleClear = () => {
+    
+    this.input1.value = '';
+    this.input2.value = '';
+    this.op.value='+';
+    this.setState({ result: 0 });
+  }
+
+
+  constructor() {
     super();
     this.state = {
       result: 0
     }
     this.handleClick = this.handleClick.bind(this);
+    this.handleClear = this.handleClear.bind(this);
   }
-  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Calculator</h1>
         </header>
         <p className="App-intro">
-          <input ref={input => this.input1 = input} type="number"/>
+          <input ref={input => this.input1 = input} type="number" />
           <select ref={input => this.op = input}>
+          
             <option value="+">+</option>
             <option value="-">-</option>
             <option value="x">x</option>
             <option value="/">/</option>
           </select>
-          <input ref={input => this.input2 = input} type="number"/>
+          <input ref={input => this.input2 = input} type="number" />
           <button onClick={this.handleClick}>=</button>
           <label className="App-result">{this.state.result}</label>
+          <br/>
+          <button onClick={this.handleClear}> Clear</button>
         </p>
       </div>
     );
